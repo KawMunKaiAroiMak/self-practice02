63a51e72a4bb4213d5e7403e86f4d715a969f963
Student <Student@Train11-4-PC068.control.ad.sit.kmutt.ac.th>
2025-09-16 15:04:29 +0700

    first commit

---
+++ week06.js
@@ -0,0 +1,27 @@
+//higher order function
+const greet = (name,formatter) => formatter(name);
+const shout = (text) => text.toUpperCase() + "!!!"
+
+console.log(greet("Palm", shout)) //PALM!!!
+
+//
+
+let y = 100 //global variable
+let sum = 5
+sum = 2
+function getScore(x) {
+  let y = 10 //local variable
+  //global function
+  let mid = 40
+  let final = 30
+  function doSomething() {
+    console.log(`dosomething: ${y + sum + x + mid + final}`)
+    //83
+    //y (global),sum(global), y (local), x(local (parameter)) mid (local), final (local)
+  }
+  doSomething()
+  return mid + final + x + y + sum
+}
+const score = getScore(1)
+console.log(score)
+// console.log(x, mid, final) //cannot access
